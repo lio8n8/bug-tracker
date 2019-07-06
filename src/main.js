cefc069bf726263be087ae64fac447a75d435d96
lio8n8 <lio8n8@gmail.com>
2019-07-06 19:16:09 +0300

    Added axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import axios from 'axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import urls from './urls';
+import './registerServiceWorker';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const axiosBase = axios.create({
+    baseURL: urls.base
+});
+
+// Vue.prototype.$http = axiosBase;
+Vue.prototype.axios = axiosBase;
 
 new Vue({
     router,
---
+++ src/urls.js
@@ -0,0 +1,5 @@
+module.exports = {
+    base: 'http://bug-tracker-api:8080',
+    users: `${this.base}/users`,
+    tasks: `${this.base}/tasks`
+}
