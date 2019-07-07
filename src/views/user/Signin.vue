bf2de9651ba11f33ed47d1e174b641127a4a5bc5
lio8n8 <lio8n8@gmail.com>
2019-07-07 20:26:05 +0300

    Signin - Initial style

--- src/views/user/Signin.vue
+++ src/views/user/Signin.vue
@@ -1,5 +1,52 @@
 <template>
     <div class="signin">
-        <h1>Not yet implemented!</h1>
+        <div class="form-wrap">
+            <h2 class="title">{{$t('common.signin')}}</h2>
+            <form v-on:submit.prevent>
+                <div class="form-item">
+                    <label for="email">{{$t('forms.emaillabel')}}:</label>
+                    <input v-model="user.email" type="email" class="form-control" id="email" />
+                    <div v-if="errors.psw" class="form-error">{{errors.email}}</div>
+                </div>
+                <div class="form-item">
+                    <label for="psw">{{$t('forms.pswlabel')}}:</label>
+                    <input v-model="user.psw" type="password" class="form-control" id="psw" />
+                    <div v-if="errors.psw" class="form-error">{{errors.psw}}</div>
+                </div>
+                <button
+                    type="submit"
+                    class="btn btn-small btn-green"
+                    @click="signin"
+                >{{$t('common.submit')}}</button>
+                <div class="link">
+                    {{$t('signin.notregistered')}}
+                    <a href="/signup">{{$t('common.signup')}}</a>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                email: '',
+                psw: ''
+            },
+            errors: {
+                email: '',
+                psw: ''
+            }
+        };
+    },
+    methods: {
+        signin() {}
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import './style.scss';
+</style>
