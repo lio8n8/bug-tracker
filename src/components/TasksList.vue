30b9ed1b79002f1a256a8005f992dc25242af49e
lio8n8 <lio8n8@gmail.com>
2019-09-01 13:34:34 +0300

    Tasks component - Start implementation

---
+++ src/components/TasksList.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="tasks">
+    <div class="task-item" v-for="task in tasks">
+      <span class="task-id">{{task.id}}</span>
+      <span class="task-title">{{task.title}}</span>
+      <span class="task-type" :class="task.type.toLowerCase()">{{task.type}}</span>
+      <span class="task-priority" :class="task.priority.toLowerCase()">{{task.priority}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tasks: [
+                {
+                    id: '3bbf4bf6-3c06-4dd6-819a-bb31379e9f64',
+                    title: 'Deleniti nostrum non qui iusto.',
+                    description:
+                        'Ratione qui pariatur numquam tenetur animi voluptatem eveniet. Dolor consequatur at et facere. Ut ex eveniet incidunt perferendis eaque ea.',
+                    priority: 'NORMAL',
+                    type: 'TASK',
+                    status: 'OPEN'
+                },
+                {
+                    id: 'ccbf0dda-3866-4f74-82bf-755341df7945',
+                    title: 'Sit voluptatem itaque corrupti laborum nostrum.',
+                    description:
+                        'Fuga optio dignissimos labore et commodi et voluptatibus. Est ipsum quidem et non omnis eligendi ut. Odio in eos veniam. Labore repellat doloremque nisi.',
+                    priority: 'CRITICAL',
+                    type: 'BUG',
+                    status: 'OPEN'
+                },
+                {
+                    id: '9db1e225-a84b-4917-b140-c4707df2feb3',
+                    title: 'Perferendis exercitationem tempora qui.',
+                    description:
+                        'Commodi et nulla nihil labore. Sit qui nihil ut ex minus. Temporibus rerum similique accusamus sunt ut.',
+                    priority: 'MAJOR',
+                    type: 'TASK',
+                    status: 'CLOSED'
+                },
+                {
+                    id: 'eeecd144-71e6-47d1-9af1-45765e68c115',
+                    title: 'Aspernatur sit laudantium.',
+                    description:
+                        'Et ut deleniti. Hic optio ullam ut aut veritatis. Aut corporis aut quibusdam eveniet. Corporis ad adipisci aut accusantium sunt voluptas.',
+                    priority: 'NORMAL',
+                    type: 'TASK',
+                    status: 'OPEN'
+                },
+                {
+                    id: 'f5059eee-33ed-4ec1-a699-e5298da84edd',
+                    title: 'Doloribus repudiandae est eos omnis inventore.',
+                    description:
+                        'Quia recusandae distinctio dolorum. Qui voluptatem sed. Et doloremque cupiditate recusandae fuga explicabo molestiae iusto. Pariatur fugiat qui. Sed et tempore.',
+                    priority: 'MINOR',
+                    type: 'BUG',
+                    status: 'OPEN'
+                },
+                {
+                    id: '92f8e2cb-b73d-45c3-865f-2f21faa56d16',
+                    title: 'Qui eius optio.',
+                    description:
+                        'Repellat odio voluptates omnis quos placeat. Cum soluta odit perferendis et nemo quasi sequi. Tempore tempora dolore sit natus. Vel debitis temporibus delectus doloremque temporibus amet et.',
+                    priority: 'NORMAL',
+                    type: 'PROPOSAL',
+                    status: 'OPEN'
+                },
+                {
+                    id: '0ef5ac1f-3b79-4103-8cae-76605488e259',
+                    title: 'Possimus omnis eaque.',
+                    description:
+                        'Vitae maxime architecto nihil natus rem alias qui. Eius qui aut et eos delectus laboriosam dolorem. Quas quos eum inventore. Cum libero necessitatibus ullam sit.',
+                    priority: 'NORMAL',
+                    type: 'TASK',
+                    status: 'OPEN'
+                },
+                {
+                    id: '63a4bc56-aa57-42c0-b446-2e00d2e1f09e',
+                    title:
+                        'Aut sed veritatis consequatur dolorum facilis explicabo.',
+                    description:
+                        'Ab et iure id non adipisci. Et qui cupiditate rerum. Voluptatum sit eligendi ut voluptas nisi autem.',
+                    priority: 'BLOCKER',
+                    type: 'TASK',
+                    status: 'OPEN'
+                },
+                {
+                    id: '9349abc8-76c9-4845-adae-4f6d3cd0558f',
+                    title: 'Aperiam fuga est odit error eligendi nesciunt vel.',
+                    description:
+                        'Aliquam et dolorem voluptas dolorem. Ut minima qui. Consequatur doloremque vel. Aperiam eligendi vel sit. Maiores voluptatum et natus inventore aut.',
+                    priority: 'NORMAL',
+                    type: 'TASK',
+                    status: 'OPEN'
+                },
+                {
+                    id: 'e4712a5f-8644-4b03-bdae-232291c300ac',
+                    title:
+                        'Eos voluptatem consequatur nostrum et ipsam ducimus.',
+                    description:
+                        'Voluptate nostrum consequatur in laudantium laborum repellendus. Incidunt qui minus qui omnis neque. Error nisi animi suscipit vitae.',
+                    priority: 'NORMAL',
+                    type: 'PROPOSAL',
+                    status: 'OPEN'
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/constants.scss';
+@import '@/assets/scss/common.scss';
+
+.tasks {
+    display: flex;
+    flex-direction: column;
+    padding: 0.4rem;
+    border: 1px solid $color-light-green-2;
+    border-radius: 0.4rem;
+}
+
+.task-item {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid $color-light-green-2;
+    height: 3rem;
+    padding: 0 0.5rem;
+
+    &:last-child {
+        border-bottom: none;
+    }
+}
+
+.task-id {
+    &:hover {
+        cursor: pointer;
+    }
+}
+
+.task-title {
+}
+
+.task-type,
+.task-priority {
+    display: flex;
+    color: #fff;
+    padding: 0.2rem 0.8rem;
+    border-radius: 0.4rem;
+}
+
+.bug {
+    background-color: $color-error;
+}
+
+.task {
+    background-color: $color-light-blue;
+}
+
+.proposal {
+    background-color: $color-light-green;
+}
+
+.normal {
+    background-color: $color-light-blue;
+}
+
+.minor {
+    background-color: $color-light-green;
+}
+
+.major {
+    background-color: $color-error;
+}
+
+.critical {
+    background-color: $color-error-red;
+}
+
+.blocker {
+    background-color: #000;
+}
+</style>
--- src/views/task/Tasks.vue
+++ src/views/task/Tasks.vue
@@ -1,5 +1,28 @@
 <template>
-    <div class="tasks">
-        <h1>Not yet implemented!</h1>
-    </div>
+  <div class="tasks">
+    <h1 class="title">{{$t('common.tasks')}}</h1>
+    <tasks />
+  </div>
 </template>
+
+<script>
+import Tasks from '../../components/TasksList';
+
+export default {
+    components: {
+        Tasks
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+    text-align: start;
+    margin: 1rem 0.8rem;
+}
+
+.tasks {
+    flex-direction: column;
+    margin: 0 0.8rem;
+}
+</style>
